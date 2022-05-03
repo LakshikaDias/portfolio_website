72b4aae7fe7e8b347e9692a1b69c09a2f5bcb868
LakshikaDias <lakshika9504@gmail.com>
2022-05-03 14:15:42 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.scss';
 import Layout from './components/Layout';
+import Home from './components/Home';
 
 function App() {
   return (
 <>
 <Routes>
     <Route path="/" element={<Layout/>}></Route>
+    <Route index element= {<Home />}/>
 </Routes>
 </>
   );
---
+++ src/components/Home/index.js
@@ -0,0 +1,29 @@
+import LogoTitle from "../../assets/images/logo-s.png";
+import {Link} from "react-router-dom";
+import './index.scss';
+
+const Home = ()  => {
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>
+                    Hi, <br/> I'm 
+                    <img src={LogoTitle} alt="developer" />
+                    dias
+                    <br/>
+                    web developer
+                </h1>
+                <h2>
+                    Frontend Developer /  UI,UX Engineer 
+                </h2>
+
+                <Link>
+                    contact me
+                </Link>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Home
