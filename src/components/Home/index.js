155b9113c7ed241537873997d6910077be1f9816
LakshikaDias <lakshika9504@gmail.com>
2022-05-15 13:09:26 +0530

    About components

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,7 @@ import './index.scss';
 import AnimatedLetters from "../AnimatedLetters";
 import { useState, useEffect} from "react";
 import Logo from "./Logo";
+import Loader from "react-loaders";
 
 const Home = ()  => {
 
@@ -33,29 +34,32 @@ const Home = ()  => {
       }, [])
 
     return (
-        <div className="container home-page">
-            <div className="text-zone">
-                <h1>
-                <span className={letterClass}>H</span>
-                <span className={`${letterClass} _12`}>i,</span> 
-                <br/>
-                <span className={`${letterClass} _13`}>I</span>
-                <span className={`${letterClass} _14`}>'m</span> 
-                    <img src={LogoTitle} alt="developer" />
-                    <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15}/>
+        <>
+            <div className="container home-page">
+                <div className="text-zone">
+                    <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span> 
                     <br/>
-                    <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={20}/>
-                </h1>
-                <h2>
-                    Frontend Developer /  UI,UX Engineer 
-                </h2>
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>'m</span> 
+                        <img src={LogoTitle} alt="developer" />
+                        <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15}/>
+                        <br/>
+                        <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={20}/>
+                    </h1>
+                    <h2>
+                        Frontend Developer /  UI,UX Engineer 
+                    </h2>
 
-                <Link to="/contact" className="flat-button">
-                    contact me
-                </Link>
+                    <Link to="/contact" className="flat-button">
+                        contact me
+                    </Link>
+                </div>
+                <Logo/>
             </div>
-            <Logo/>
-        </div>
+            <Loader type="pacman" />
+        </>
     )
 }
 
